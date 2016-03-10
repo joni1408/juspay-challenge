be0e087d6288c30dd66b5253f35212a7e8651004
Prempal Singh <prempal.42@gmail.com>
2016-03-10 20:50:28 +0530

    trying other rss providers

--- app/src/main/java/com/prempal/juspay_challenge/MainActivity.java
+++ app/src/main/java/com/prempal/juspay_challenge/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView hinduImage = (ImageView) findViewById(R.id.imageView);
+        ImageView hinduImage = (ImageView) findViewById(R.id.iv_hindu);
         Picasso.with(this).load("https://d1iiooxwdowqwr.cloudfront.net/pub/wpscreenshots/android-logo-512-512.png").into(hinduImage);
         hinduImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(view.getContext(), ArticleListActivity.class);
                 intent.putExtra("title", "The Hindu");
                 intent.putExtra("link", "http://www.thehindu.com/news/cities/Delhi/?service=rss");
+                startActivity(intent);
+            }
+        });
+
+        ImageView timesImage = (ImageView) findViewById(R.id.iv_toi);
+        Picasso.with(this).load("http://ecx.images-amazon.com/images/I/51ANlB4VuvL.png").into(timesImage);
+        timesImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ArticleListActivity.class);
+                intent.putExtra("title", "Times of India");
+                intent.putExtra("link", "http://timesofindia.feedsportal.com/c/33039/f/533976/index.rss");
                 startActivity(intent);
             }
         });
